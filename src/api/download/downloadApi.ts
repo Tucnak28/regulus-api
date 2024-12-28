519312761220f546c177121ffa40ee2cc57932df
Stanislav Haluska <stanislav.haluska@gmail.com>
2024-12-29 00:32:49 +0100

    lint fix

--- src/api/download/downloadApi.ts
+++ src/api/download/downloadApi.ts
@@ -6,7 +6,7 @@ export class DownloadApi {
   public async fetchFile(apiUrl: string): Promise<AxiosResponse> {
     if (!softPLCCookie) {
       const lResponse = await LoginService.getLogin(); // initialize cookie SoftPLC
-      const suc = await LoginService.successLogin(lResponse);
+      await LoginService.successLogin(lResponse);
     }
 
     return await axiosInstance.get(apiUrl, {
